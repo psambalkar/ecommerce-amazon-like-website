6ab97f427ebc562e05ef55ac833d04bb12f9ad2c
psambalkar <parth.ambalkar16@gmail.com>
2020-08-14 00:21:39 +0530

    commit homepage into smaller components and finish styling menu items components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      
        <HomePage/> 
-      </div>
+      
     );
   }
 }
