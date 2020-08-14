64598c5d5b5b5140371dbd329b7c8870a7c051d1
psambalkar <parth.ambalkar16@gmail.com>
2020-08-14 14:03:25 +0530

    commit header component adding links to home

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-import {Route}  from 'react-router-dom';
-import ShopPage from "./pages/shoppage/shop.component"
+import {Route,Switch}  from 'react-router-dom';
+import ShopPage from "./pages/shoppage/shop.component";
+import Header from "./components/header/header.component"
    
 class App extends Component {
   render() {
     return (
     <div>
+      <Header/> 
+     <Switch>
       <Route exact={true} path="/" component={HomePage}></Route>
-      <Route exact={false} path="/shop" component={ShopPage}></Route>
+      <Route  path="/shop" component={ShopPage}></Route>
+      </Switch>
     </div>        
     );
   }
